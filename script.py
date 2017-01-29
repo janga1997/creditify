386a44af984ac0e32870f787d26c2a2a82db0b7c
janga <janga1997@gmail.com>
2017-01-29 13:37:47 +0530

    Add support for HTML files

--- script.py
+++ script.py
@@ -1,18 +1,31 @@
+#! /usr/bin/env python
+
 import sys
-
-directory = sys.argv[1]
-author = sys.argv[2]
-
 import os
+
+from bs4 import BeautifulSoup
+
+filetype = sys.argv[1]
+directory = sys.argv[2]
+author = sys.argv[3]
 
 for dname, dirs,files in os.walk(directory):
     for file in files:
-        if file.endswith('.py'):
-            print '\n'
+        if file.endswith('.'+filetype):
             f = open(os.path.join(dname, file),'r+')
-            lines = f.readlines() # read old content
-            f.seek(0) # go back to the beginning of the file
-            f.write("__author__ = '"+ author +"'\n\n") # write new content at the beginning
-            for line in lines: # write old content after new
-                f.write(line)
+            if filetype == 'py':
+                lines = f.readlines() # read old content
+                f.seek(0) # go back to the beginning of the file
+                f.write("__author__ = '"+ author +"'\n\n") # write new content at the beginning
+                for line in lines: # write old content after new
+                    f.write(line)
+
+            elif filetype == 'html':
+                soup = BeautifulSoup(f.read())
+                title = soup.find('title')
+                meta = soup.new_tag('meta')
+                meta['author'] = author
+                title.insert_after(meta)
+                f.seek(0)
+                f.write(soup.prettify())
             f.close()
